e6e74ecd485dffe2865367a2504d328cf42b4b5f
Jérémy Cloarec <105861264+Jeremy-Cloarec@users.noreply.github.com>
2024-05-02 14:04:30 +0200

    test

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { styles } from '../styles';
 export default function Header() {
     return (
         <View>
+            <Text style={styles.h1}>Rompez avec la routine!</Text>
             <Text style={styles.h1}>Hello Mystifolie !</Text>
         </View>
     )
